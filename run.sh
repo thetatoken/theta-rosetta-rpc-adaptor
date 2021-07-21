056636d881e1894a126cf74a99f486167129467a
Qinwei Gong <qinwei@sliver.tv>
2021-07-21 13:50:52 -0700

    d

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+echo "consensus:
+  minProposalWait: 2" >> ../privatenet/node/config.yaml
+
+cat ../privatenet/node/config.yaml
 /go/bin/theta start --config=../privatenet/node --password="qwertyuiop" &
 
-cd ~/.thetacli/keys/encrypted/
-ls
-pwd
-whoami
 # cp /go/src/github.com/thetatoken/privatenet/node/key/encrypted/* ~/.thetacli/keys/encrypted/
-sleep 10
+sleep 30
 /go/bin/thetacli query status
 /go/bin/theta-rosetta-rpc-adaptor start
