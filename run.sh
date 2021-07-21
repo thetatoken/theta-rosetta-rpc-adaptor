f65275f240f5169a384ed3f0271cd02975092c3f
Qinwei Gong <qinwei@sliver.tv>
2021-07-21 13:21:56 -0700

    d

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 /go/bin/theta start --config=../privatenet/node --password="qwertyuiop" &
-# ls /go/src/github.com/thetatoken/privatenet/node/key/encrypted
+
 ls ~/.thetacli/keys/encrypted/
-ls /go/src/github.com/thetatoken/privatenet/node/config.yaml
+whoami
 # cp /go/src/github.com/thetatoken/privatenet/node/key/encrypted/* ~/.thetacli/keys/encrypted/
 sleep 10
 /go/bin/thetacli query status
