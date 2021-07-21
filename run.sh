8261c5f03e9a7e6fda94bd4c8ed4654436f5429a
Qinwei Gong <qinwei@sliver.tv>
2021-07-21 13:11:00 -0700

    d

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 /go/bin/theta start --config=../privatenet/node --password="qwertyuiop" &
-sleep 1
-cp /go/src/github.com/thetatoken/privatenet/node/key/encrypted/* ~/.thetacli/keys/encrypted/
-sleep 15
+# ls /go/src/github.com/thetatoken/privatenet/node/key/encrypted
+ls ~/.thetacli/keys/encrypted/
+ls /go/src/github.com/thetatoken/privatenet/node/config.yaml
+# cp /go/src/github.com/thetatoken/privatenet/node/key/encrypted/* ~/.thetacli/keys/encrypted/
+sleep 10
 /go/bin/thetacli query status
 /go/bin/theta-rosetta-rpc-adaptor start
